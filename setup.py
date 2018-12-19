5a8186dc08c30e7f6b85ce98ffbaaf9b6b6922fe
varUP <aaron.reyna@gmail.com>
2018-12-18 16:05:42 -0800

    renaming 'py_modules' to 'packages' in setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 
 setup(
     name='snap-numpy-aaron',
-    version='1.0.0',
+    version='1.0',
     author='aaron',
     author_email='aaron@fake_email.com',
     description='simple snap / numpy demo',
     url='https://github.com/varUP/snap_numpy_demo_aaron.git',
-    py_modules=['snap-numpy-aaron'],
+    packages=['snap-numpy-aaron'],
     scripts=['bin/snap_numpy_aaron.py'],
     install_requires=['numpy'],
     license='License :: MIT License',
