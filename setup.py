da9c0db9ee08bfb9fb98241d8ffa0058b4cd8720
varUP <aaron.reyna@gmail.com>
2018-12-18 16:09:39 -0800

    changed values in setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email='aaron@fake_email.com',
     description='simple snap / numpy demo',
     url='https://github.com/varUP/snap_numpy_demo_aaron.git',
-    packages=['snap-numpy-aaron'],
+    packages=['snap_numpy_aaron'],
     scripts=['bin/snap_numpy_aaron.py'],
     install_requires=['numpy'],
     license='License :: MIT License',
